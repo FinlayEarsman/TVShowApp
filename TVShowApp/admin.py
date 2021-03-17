0390bfd5b386522dc480febd6e3a3d3e90da1355
Ethan Todd <2464490T@student.gla.ac.uk>
2021-03-17 11:20:39 +0000

    Added Models

--- TVShowApp/admin.py
+++ TVShowApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from TVShowApp.models import Genre, Show, Belonging, Review
 
 # Register your models here.
+admin.site.register(Genre)
+admin.site.register(Show)
+admin.site.register(Belonging)
+admin.site.register(Review)
--- TVShowApp/models.py
+++ TVShowApp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length = 50, unique = True)
+
+    def __str__(self):
+        return self.name
+
+class Show(models.Model):
+    id = models.IntegerField(unique=True,primary_key=True)
+    title = models.CharField(max_length = 100)
+    year = models.IntegerField()
+    photo = models.ImageField(upload_to="show_images" ,blank=True)
+
+    def __str__(self):
+        return self.title
+
+class Belonging(models.Model):
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    show = models.ForeignKey(Show, on_delete=models.CASCADE)
+
+class Review(models.Model):
+    show = models.ForeignKey(Show, on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    id = models.IntegerField(unique=True,primary_key=True)
+    comment = models.CharField(max_length = 500)
+    rating = models.IntegerField()
