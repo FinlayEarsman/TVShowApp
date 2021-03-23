e99bac35a5b4360826676d28dae9f19b77f57fb6
Binfeng Cheng <2507051c@student.gla.ac.uk>
2021-03-23 12:12:02 +0800

    add genres
    add and delete method for genres

--- TVShowApp/admin.py
+++ TVShowApp/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from TVShowApp.models import Genre, Show, Belonging, Review
 
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
 # Register your models here.
-admin.site.register(Genre)
+admin.site.register(Genre, GenreAdmin)
 admin.site.register(Show)
 admin.site.register(Belonging)
 admin.site.register(Review)
