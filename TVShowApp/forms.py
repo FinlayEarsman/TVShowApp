43ef8ac429eac13d0445cc979439c5d15592259e
Binfeng Cheng <2507051c@student.gla.ac.uk>
2021-03-29 22:25:43 +0800

    show and review forms
    update

--- TVShowApp/forms.py
+++ TVShowApp/forms.py
@@ -13,8 +13,24 @@ class GenreForm(forms.ModelForm):
         model = Genre
         fields = ('name',)
 
+<<<<<<< Updated upstream
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
     class Meta:
         model = User
         fields = ('username','email','password',)
+=======
+
+class ShowForm(forms.ModelForm):
+    title = forms.CharField(max_length=100,
+                           help_text="Please enter the titke of the show.")
+    year = forms.IntegerField(help_text="Please enter the year of the show.")
+    photo = forms.ImageField(help_text="Please upload a photo for the show.")
+    avg_rating = forms.FloatField(help_text="Please enter the rate of the show.")
+
+
+class ReviewForm(forms.ModelForm):
+    comment = forms.CharField(max_length=500)
+    rating = forms.IntegerField()
+
+>>>>>>> Stashed changes
